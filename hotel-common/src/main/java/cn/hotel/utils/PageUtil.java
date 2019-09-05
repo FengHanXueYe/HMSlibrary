70af54ab2fac8d52df136d0c5e606fcc55fc5aef
admin <937518043@qq.com>
2019-09-05 16:33:24 +0800

    实体

---
+++ hotel-common/src/main/java/cn/hotel/utils/PageUtil.java
@@ -0,0 +1,52 @@
+package cn.hotel.utils;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class PageUtil<T> implements Serializable {
+    private Integer pageNo;
+    private Integer pageSize;
+    private Integer totalPageCount;
+    private int[] navigatepageNums;
+    private List<T> list;
+
+    public Integer getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(Integer pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Integer getTotalPageCount() {
+        return totalPageCount;
+    }
+
+    public void setTotalPageCount(Integer totalPageCount) {
+        this.totalPageCount = totalPageCount;
+    }
+
+    public int[] getNavigatepageNums() {
+        return navigatepageNums;
+    }
+
+    public void setNavigatepageNums(int[] navigatepageNums) {
+        this.navigatepageNums = navigatepageNums;
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+}
