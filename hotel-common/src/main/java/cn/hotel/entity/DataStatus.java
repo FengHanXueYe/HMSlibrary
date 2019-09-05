269ae286f098841ab612d1f6fc2ce09735748b68
admin <937518043@qq.com>
2019-09-05 14:23:54 +0800

    实体

---
+++ hotel-common/src/main/java/cn/hotel/entity/DataStatus.java
@@ -0,0 +1,79 @@
+package cn.hotel.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class DataStatus implements Serializable {
+    private Integer id;
+    private String typeCode;
+    private Integer valueId;
+    private String valueName;
+    private Integer createdBy;
+    private Date creationDate;
+    private Integer modifyBy;
+    private Date modifyDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTypeCode() {
+        return typeCode;
+    }
+
+    public void setTypeCode(String typeCode) {
+        this.typeCode = typeCode;
+    }
+
+    public Integer getValueId() {
+        return valueId;
+    }
+
+    public void setValueId(Integer valueId) {
+        this.valueId = valueId;
+    }
+
+    public String getValueName() {
+        return valueName;
+    }
+
+    public void setValueName(String valueName) {
+        this.valueName = valueName;
+    }
+
+    public Integer getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Integer createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Integer getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(Integer modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+}
