a3be42a19443454f096e4b3c98a4b8b6c911345b
admin <937518043@qq.com>
2019-09-08 17:45:47 +0800

    实体

--- hotel-provider/src/main/java/cn/hotel/hms/service/RoomServiceImpl.java
+++ hotel-provider/src/main/java/cn/hotel/hms/service/RoomServiceImpl.java
@@ -1,9 +1,11 @@
 package cn.hotel.hms.service;
 
+import cn.hotel.entity.DataStatus;
 import cn.hotel.entity.Room;
 import cn.hotel.hms.mapper.RoomMapper;
 import cn.hotel.service.RoomService;
 import cn.hotel.utils.PageUtil;
+import cn.hotel.vo.RoomVO;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -18,11 +20,11 @@ public class RoomServiceImpl implements RoomService {
     private RoomMapper roomMapper;
 
     @Override
-    public PageUtil<Room> queryAll(Room room, Integer pageNum, Integer pageSize) {
+    public PageUtil<RoomVO> queryAll(Room room, Integer pageNum, Integer pageSize) {
         PageHelper.startPage(pageNum,pageSize);
-        List<Room> list = roomMapper.queryAll(room);
-        PageInfo<Room> pageInfo =new PageInfo<>(list);
-        PageUtil<Room> pageUtil =new PageUtil<>();
+        List<RoomVO> list = roomMapper.queryAll(room);
+        PageInfo<RoomVO> pageInfo =new PageInfo<>(list);
+        PageUtil<RoomVO> pageUtil =new PageUtil<>();
         pageUtil.setPageNo(pageInfo.getPageNum());
         pageUtil.setPageSize(pageInfo.getPageSize());
         pageUtil.setTotalPageCount(pageInfo.getPages());
@@ -31,4 +33,12 @@ public class RoomServiceImpl implements RoomService {
 
         return pageUtil;
     }
+
+
+    @Override
+    public RoomVO queryByNumber(String no) {
+        return roomMapper.queryByNumber(no);
+    }
+
+
 }
