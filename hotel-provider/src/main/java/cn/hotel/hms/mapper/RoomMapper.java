d7ae7714496add9204e40f562c02174c1e94b905
admin <937518043@qq.com>
2019-09-08 17:45:18 +0800

    实体

--- hotel-provider/src/main/java/cn/hotel/hms/mapper/RoomMapper.java
+++ hotel-provider/src/main/java/cn/hotel/hms/mapper/RoomMapper.java
@@ -1,11 +1,17 @@
 package cn.hotel.hms.mapper;
 
+import cn.hotel.entity.DataStatus;
 import cn.hotel.entity.Room;
 import cn.hotel.utils.PageUtil;
+import cn.hotel.vo.RoomVO;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface RoomMapper {
     //条件查询所有客房
-    List<Room> queryAll(Room room);
+    List<RoomVO> queryAll(Room room);
+
+    //查询当前客房综合信息
+    RoomVO queryByNumber(String no);
 }
