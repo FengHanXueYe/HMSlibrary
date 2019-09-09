f82d9b84f2a017cda3fa7753b3f63a503cdcfe76
zhangzhaoxinga <2684849329@qq.com>
2019-09-09 12:16:38 +0800

    食品操作

--- hotel-consumer/src/main/java/cn/hotel/hms/controller/DeliciousFoodController.java
+++ hotel-consumer/src/main/java/cn/hotel/hms/controller/DeliciousFoodController.java
@@ -10,6 +10,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 
 @Controller
 public class DeliciousFoodController {
@@ -33,5 +38,50 @@ public class DeliciousFoodController {
             System.out.println(""+food);
         }
         return JSON.toJSONString(deliciousFoodVoPageUtil);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "DetailDelicious", method = RequestMethod.POST, produces = {"application/json;charset=utf-8"})
+    public String detailDelicious(Integer id){
+        DeliciousFoodVo deliciousFoodVo=deliciousFoodService.detailDeliciousFood(id);
+        return JSON.toJSONString(deliciousFoodVo);
+    }
+
+
+    @ResponseBody
+    @RequestMapping(value = "DeleteDelicious", method = RequestMethod.POST)
+    public boolean deleteDelicious(Integer id){
+        Integer delicious=deliciousFoodService.deleteDeliciousFood(id);
+        return delicious>0?true:false;
+    }
+
+
+    @RequestMapping(value = "toAddDelicious")
+    public String toAddDelicious(){
+        return "food/add_food";
+    }
+
+    @RequestMapping(value = "doAddDelicious")
+    public String doAddDelicious(DeliciousFood deliciousFood){
+        this.deliciousFoodService.addDeliciousFood(deliciousFood);
+        return "redirect:queryAllFood";
+    }
+
+
+    private String path="images";//要保存的文件夹的名字,需修改
+    @RequestMapping(value="fileController",produces = "text/html;charset=UTF-8")//解决返回中文乱码
+    @ResponseBody//设置ajax 返回保存路径
+    //
+    public String fileController(MultipartFile file, HttpServletRequest request, HttpServletResponse response) {
+        response.setCharacterEncoding("utf-8");
+        //寻找要保存的文件夹(保存路径)
+        String savePath = request.getSession().getServletContext().getRealPath("/"+path+"/");
+        try {
+            file.transferTo(new File(savePath+"/"+file.getOriginalFilename()));
+        //在"/"后面可以自行拼接name或加入随机数(以免不同用户上传替换)
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "/"+path+"/"+file.getOriginalFilename();
     }
 }
